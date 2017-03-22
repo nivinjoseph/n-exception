129d93450ce6f091242862d685be88b55b6df74d
Viola Deluca <viola__r@hotmail.com>
2017-03-22 16:03:24 -0400

    made changes to implementing-tests

--- test/exception.test.js
+++ test/exception.test.js
@@ -7,8 +7,34 @@ suite("exceptions", function () {
     suite("ApplicationException", function () {
         test("should have the right exceptionType", function () {
             var exp = new index_1.ApplicationException("test exception");
-            var exceptionType = exp.exceptionType;
             assert.strictEqual(exp.exceptionType, "ApplicationException");
+        });
+        test("should display <none> when message is not provided", function () {
+            var innerExp = "Hello";
+            var exp = new index_1.ApplicationException("");
+            assert.strictEqual(exp.message, "<none>");
+        });
+        test("should display <none> when message is a white space", function () {
+            var exp = new index_1.ApplicationException(" ");
+            assert.strictEqual(exp.message, "<none>");
+        });
+        test("should display <none> when message is an empty string", function () {
+            var exp = new index_1.ApplicationException("");
+            assert.strictEqual(exp.message, "<none>");
+        });
+        test.only("should return the message value when message argument is provided", function () {
+            var exp = new index_1.ApplicationException("You have an");
+            assert.strictEqual(exp.message, "You have an error");
+        });
+        test("should display null if no innerException argument is provided");
+        test("should return Exception when innerException argument is provided");
+        test("should display ");
+        test("should return a string value if error exists");
+        test("should return ..... if error does not exist");
+        test("", function () {
+            var innerExp = new index_1.ApplicationException("hello");
+            var exp = new index_1.ApplicationException("hi", innerExp);
+            assert.strictEqual(exp.innerException, innerExp);
         });
     });
 });
--- test/exception.test.ts
+++ test/exception.test.ts
@@ -19,9 +19,49 @@ suite("exceptions", () =>
             assert.strictEqual(exp.exceptionType, "ApplicationException");
         });
         
+        test("should display <none> when message is not provided", () =>
+        {
+            let innerExp = "Hello";
+            let exp = new ApplicationException("");
+            
+            assert.strictEqual(exp.message, "<none>");
+        });
+        
+        test("should display <none> when message is a white space", () =>
+        {
+            let exp = new ApplicationException(" ");
+            
+            assert.strictEqual(exp.message, "<none>");
+        });
+        
+        test("should display <none> when message is an empty string", () =>
+        {
+            let exp = new ApplicationException("");
+            
+            assert.strictEqual(exp.message, "<none>");
+        });
+        
+        test.only("should return the message value when message argument is provided", () =>
+        {
+            let exp = new ApplicationException("You have an error"); 
+            
+            assert.strictEqual(exp.message, "You have an error");
+        });
+        
+        test("should display null if no innerException argument is provided");
+        test("should return Exception when innerException argument is provided");
+        test("should display ");
+        
+        test("should return a string value if error exists");
+        test("should return ..... if error does not exist");
+        
         test("", () =>
         {
+            let innerExp = new ApplicationException("hello");   
+            let exp = new ApplicationException("hi", innerExp);
             
+            assert.strictEqual(exp.innerException, innerExp);
         });
+        
     });
 });
