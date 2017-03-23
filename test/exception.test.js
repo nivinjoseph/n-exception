6d04c8a02e8fda308530a9d70f1bd67e72741225
Viola Deluca <viola__r@hotmail.com>
2017-03-23 14:10:10 -0400

    completed all exception testing

--- test/exception.test.js
+++ test/exception.test.js
@@ -9,11 +9,6 @@ suite("exceptions", function () {
             var exp = new index_1.ApplicationException("test exception");
             assert.strictEqual(exp.exceptionType, "ApplicationException");
         });
-        test("should display <none> when message is not provided", function () {
-            var innerExp = "Hello";
-            var exp = new index_1.ApplicationException("");
-            assert.strictEqual(exp.message, "<none>");
-        });
         test("should display <none> when message is a white space", function () {
             var exp = new index_1.ApplicationException(" ");
             assert.strictEqual(exp.message, "<none>");
@@ -22,19 +17,86 @@ suite("exceptions", function () {
             var exp = new index_1.ApplicationException("");
             assert.strictEqual(exp.message, "<none>");
         });
-        test.only("should return the message value when message argument is provided", function () {
-            var exp = new index_1.ApplicationException("You have an");
+        test("should return the message value when message argument is provided", function () {
+            var exp = new index_1.ApplicationException("You have an error");
             assert.strictEqual(exp.message, "You have an error");
         });
-        test("should display null if no innerException argument is provided");
-        test("should return Exception when innerException argument is provided");
-        test("should display ");
-        test("should return a string value if error exists");
-        test("should return ..... if error does not exist");
-        test("", function () {
-            var innerExp = new index_1.ApplicationException("hello");
-            var exp = new index_1.ApplicationException("hi", innerExp);
-            assert.strictEqual(exp.innerException, innerExp);
+        test("should return null if no innerException argument is provided", function () {
+            var exp = new index_1.ApplicationException("404");
+            assert.strictEqual(exp.innerException, null);
+        });
+        test("should return Exception object when innerException argument is provided", function () {
+            var innerExp = new index_1.ApplicationException("401");
+            var exp = new index_1.ApplicationException("404", innerExp);
+            assert.strictEqual(exp.innerException, { _message: "401", _innerException: null });
+        });
+        test("should return message preappended with the string Error", function () {
+            var exp = new index_1.ApplicationException("404");
+            assert.strictEqual(exp.stackTrace, "Error: 404");
+        });
+    });
+    suite("ArgumentException", function () {
+        test("should return message formated when provided argName and reason", function () {
+            var exp = new index_1.ArgumentException("i", "is valid");
+            assert.strictEqual(exp.message, "Argument 'i' is valid.");
+        });
+        test("should return argName and reason with default value when argName and reason are empty strings", function () {
+            var exp = new index_1.ArgumentException("", "");
+            assert.strictEqual(exp.message, "Argument '<UNKNOWN>' is not valid.");
+        });
+        test("should return argName and reason with default value when argName and reason are space characters", function () {
+            var exp = new index_1.ArgumentException(" ", " ");
+            assert.strictEqual(exp.message, "Argument '<UNKNOWN>' is not valid.");
+        });
+        test("should return argName with default value and formated reason when argName is an empty string and reason was provided", function () {
+            var exp = new index_1.ArgumentException("", "is valid");
+            assert.strictEqual(exp.message, "Argument '<UNKNOWN>' is valid.");
+        });
+        test("should return formated argName and default reason value when argName was provided and reason is an empty string", function () {
+            var exp = new index_1.ArgumentException("i", "");
+            assert.strictEqual(exp.message, "Argument 'i' is not valid.");
+        });
+    });
+    suite("ArgumentNullException", function () {
+        test("should return formated message when argName is provided", function () {
+            var exp = new index_1.ArgumentNullException("i");
+            assert.strictEqual(exp.message, "Argument 'i' is NULL.");
+        });
+        test("should return argName with default value when argName is an empty string", function () {
+            var exp = new index_1.ArgumentNullException("");
+            assert.strictEqual(exp.message, "Argument '<UNKNOWN>' is NULL.");
+        });
+        test("should return argName with default value when argName is a space character", function () {
+            var exp = new index_1.ArgumentNullException(" ");
+            assert.strictEqual(exp.message, "Argument '<UNKNOWN>' is NULL.");
+        });
+    });
+    suite("InvalidArgumentException", function () {
+        test("should return formated message when argName is provided", function () {
+            var exp = new index_1.InvalidArgumentException("i");
+            assert.strictEqual(exp.message, "Argument 'i' is invalid");
+        });
+        test("should return argName with default value when argName is an empty string", function () {
+            var exp = new index_1.InvalidArgumentException("");
+            assert.strictEqual(exp.message, "Argument '<UNKNOWN>' is invalid");
+        });
+        test("should return argName with default value whe argName is a space character", function () {
+            var exp = new index_1.InvalidArgumentException(" ");
+            assert.strictEqual(exp.message, "Argument '<UNKNOWN>' is invalid");
+        });
+    });
+    suite.only("InvalidOperationException", function () {
+        test("should return formated message when operation is provided", function () {
+            var exp = new index_1.InvalidOperationException("i");
+            assert.strictEqual(exp.message, "Operation 'i' is invalid.");
+        });
+        test("should return default operation value when operation is an empty string", function () {
+            var exp = new index_1.InvalidOperationException("");
+            assert.strictEqual(exp.message, "Operation '<UNKNOWN>' is invalid.");
+        });
+        test("should return default operation value when operation is a space character", function () {
+            var exp = new index_1.InvalidOperationException(" ");
+            assert.strictEqual(exp.message, "Operation '<UNKNOWN>' is invalid.");
         });
     });
 });
--- test/exception.test.ts
+++ test/exception.test.ts
@@ -16,15 +16,8 @@ suite("exceptions", () =>
         test("should have the right exceptionType", () =>
         {
             let exp = new ApplicationException("test exception");
-            assert.strictEqual(exp.exceptionType, "ApplicationException");
-        });
-        
-        test("should display <none> when message is not provided", () =>
-        {
-            let innerExp = "Hello";
-            let exp = new ApplicationException("");
             
-            assert.strictEqual(exp.message, "<none>");
+            assert.strictEqual(exp.exceptionType, "ApplicationException");
         });
         
         test("should display <none> when message is a white space", () =>
@@ -41,27 +34,146 @@ suite("exceptions", () =>
             assert.strictEqual(exp.message, "<none>");
         });
         
-        test.only("should return the message value when message argument is provided", () =>
+        test("should return the message value when message argument is provided", () =>
         {
             let exp = new ApplicationException("You have an error"); 
             
             assert.strictEqual(exp.message, "You have an error");
         });
         
-        test("should display null if no innerException argument is provided");
-        test("should return Exception when innerException argument is provided");
-        test("should display ");
-        
-        test("should return a string value if error exists");
-        test("should return ..... if error does not exist");
-        
-        test("", () =>
+        test("should return null if no innerException argument is provided", () =>
         {
-            let innerExp = new ApplicationException("hello");   
-            let exp = new ApplicationException("hi", innerExp);
+            let exp = new ApplicationException("404");
             
-            assert.strictEqual(exp.innerException, innerExp);
+            assert.strictEqual(exp.innerException, null);
         });
         
+        test("should return Exception object when innerException argument is provided", () =>
+        {
+            let innerExp = new ApplicationException("401");
+            let exp = new ApplicationException("404", innerExp);    
+            
+            assert.strictEqual(exp.innerException, { _message: "401", _innerException: null });
+        });
+        
+        test("should return message preappended with the string Error", () =>
+        {
+            let exp = new ApplicationException("404");
+            
+            assert.strictEqual(exp.stackTrace, "Error: 404");
+        });
+        
+    });
+    
+    suite("ArgumentException", () =>
+    {        
+        test("should return message formated when provided argName and reason", () =>
+        {
+            let exp = new ArgumentException("i", "is valid");
+            
+            assert.strictEqual(exp.message, "Argument 'i' is valid.");
+        }); 
+        
+        test("should return argName and reason with default value when argName and reason are empty strings", () =>
+        {
+            let exp = new ArgumentException("", "");
+            
+            assert.strictEqual(exp.message, "Argument '<UNKNOWN>' is not valid.");
+        });
+        
+        test("should return argName and reason with default value when argName and reason are space characters", () =>
+        {
+            let exp = new ArgumentException(" ", " ");
+            
+            assert.strictEqual(exp.message, "Argument '<UNKNOWN>' is not valid.");
+        });
+        
+        test("should return argName with default value and formated reason when argName is an empty string and reason was provided", () =>
+        {
+            let exp = new ArgumentException("", "is valid");
+
+            assert.strictEqual(exp.message, "Argument '<UNKNOWN>' is valid.");
+        });
+        
+        test("should return formated argName and default reason value when argName was provided and reason is an empty string", () =>
+        {
+            let exp = new ArgumentException("i", "");
+            
+            assert.strictEqual(exp.message, "Argument 'i' is not valid.");
+        });
+        
+
+    });
+    
+    suite("ArgumentNullException", () =>
+    {
+        test("should return formated message when argName is provided", () =>
+        {
+            let exp = new ArgumentNullException("i");
+            
+            assert.strictEqual(exp.message, "Argument 'i' is NULL.");
+        });
+        
+        test("should return argName with default value when argName is an empty string", () =>
+        {
+            let exp = new ArgumentNullException("");
+            
+            assert.strictEqual(exp.message, "Argument '<UNKNOWN>' is NULL.");
+        });
+        
+        test("should return argName with default value when argName is a space character", () =>
+        {
+            let exp = new ArgumentNullException(" ");
+            
+            assert.strictEqual(exp.message, "Argument '<UNKNOWN>' is NULL.");
+        });
+    });
+    
+    suite("InvalidArgumentException", () =>
+    {
+        test("should return formated message when argName is provided", () =>
+        {
+            let exp = new InvalidArgumentException("i");
+            
+            assert.strictEqual(exp.message, "Argument 'i' is invalid");
+        });
+        
+        test("should return argName with default value when argName is an empty string", () =>
+        {
+            let exp = new InvalidArgumentException("");
+            
+            assert.strictEqual(exp.message, "Argument '<UNKNOWN>' is invalid");
+        });
+        
+        test("should return argName with default value whe argName is a space character", () =>
+        {
+            let exp = new InvalidArgumentException(" ");
+            
+            assert.strictEqual(exp.message, "Argument '<UNKNOWN>' is invalid");
+        });
+    });
+    
+    suite("InvalidOperationException", () =>
+    {
+        test("should return formated message when operation is provided", () =>
+        {
+            let exp = new InvalidOperationException("i");
+            
+            assert.strictEqual(exp.message, "Operation 'i' is invalid.");
+        });
+        
+        test("should return default operation value when operation is an empty string", () =>
+        {
+            let exp = new InvalidOperationException("");
+            
+            assert.strictEqual(exp.message, "Operation '<UNKNOWN>' is invalid.");
+        });
+        
+        test("should return default operation value when operation is a space character", () =>
+        {
+            let exp = new InvalidOperationException(" ");
+            
+            assert.strictEqual(exp.message, "Operation '<UNKNOWN>' is invalid.");
+        });
     });
 });
