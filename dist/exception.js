a64333535e87f61869b8f17aae916596f994f5b3
nivinjoseph <nivinjoseph@outlook.com>
2018-10-02 09:23:27 -0400

    preparing to publish new version

--- dist/exception.js
+++ dist/exception.js
@@ -3,8 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("@nivinjoseph/n-ext");
 class Exception extends Error {
     get name() { return this._name; }
-    // public get message(): string { return this._message; }
-    // public get stack(): string { return this._stack; }
     get innerException() { return this._innerException; }
     constructor(message, innerException) {
         if (message == null || message.isEmptyOrWhiteSpace())
@@ -13,20 +11,6 @@ class Exception extends Error {
         this.message = message;
         this._name = this.getTypeName();
         this._innerException = innerException ? innerException : null;
-        // if ((<any>message) instanceof Error)
-        // {
-        //     let err = (<any>message) as Error;
-        //     this._message = err.message;
-        //     this._stack = err.stack;
-        // }
-        // else
-        // {
-        //     if (message == null || message.isEmptyOrWhiteSpace())
-        //         message = "<none>";
-        //     this._message = message;
-        //     this._stack = this.generateStackTrace();
-        //     this._innerException = innerException ? innerException : null;
-        // }
     }
 }
 exports.Exception = Exception;
